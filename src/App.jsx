bd8ff493471b827bcdea4da8b15c6eb23cb02acf
prasanth45bit <prasanthpalanisamy4@gmail.com>
2025-12-11 23:15:05 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import ProductListing from "./pages/ProductListing";
 
 export default function App() {
